f00f973f7be0b7e6d22d36a7a79c29fd0d30a63f
fajtaiandris <andris.fajkusz@gmail.com>
2021-04-03 10:05:14 +0200

    Added title metadata

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir out
-pandoc -s --template template.html content/index.html -o out/index.html
-pandoc -s --template template.html content/rolunk.html -o out/rolunk.html
+pandoc -s --template template.html -M title="Kezdőlap" content/index.html -o out/index.html
+pandoc -s --template template.html -M title="Rólunk" content/rolunk.html -o out/rolunk.html
