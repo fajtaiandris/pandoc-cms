f2ab472ed50446342f27411c3ce9d3b47b72cebc
Fajtai Andris <andris.fajkusz@gmail.com>
2021-05-22 11:58:36 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir out
-pandoc -s --template template.html -M title="Kezdőlap" content/index.md -o out/index.html
-pandoc -s --template template.html -M title="Rólunk" content/rolunk.md -o out/rolunk.html
+pandoc -s --template template.html content/index.md -o out/index.html
+pandoc -s --template template.html content/rolunk.md -o out/rolunk.html
