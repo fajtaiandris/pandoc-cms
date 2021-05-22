45349e870eab44f86f47abd4f7ac1db14de0e39a
Fajtai Andris <andris.fajkusz@gmail.com>
2021-05-22 12:25:34 +0200

    Generating sites automatically

--- compile.sh
+++ compile.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
 mkdir out
-pandoc -s --template template.html content/index.md -o out/index.html
-pandoc -s --template template.html content/rolunk.md -o out/rolunk.html
+
+# Generating sites manually
+# pandoc -s --template template.html content/index.md -o out/index.html
+# pandoc -s --template template.html content/rolunk.md -o out/rolunk.html
+
+# Generating sites automatically
+contentfiles=`ls ./content/*.md`
+for infile in $contentfiles
+do
+    base=`basename -s .md ${infile}`
+    outfile="out/${base}.html"
+    pandoc -s --template template.html $infile -o $outfile
+done
